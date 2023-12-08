33029fbcb0e2b04c6d65be51e7eb0e9f5c42614e
Jaffar Abdulhussain <ajaffar969@gmail.com>
2023-12-08 13:24:51 +0300

    Add LoginTest and Modify Login

--- Use cases/Log-in/Login.php
+++ Use cases/Log-in/Login.php
@@ -1,55 +1,57 @@
 <?php
-class LoginUseCase
+
+// Import the necessary classes
+require_once 'Use cases/DbConnection.php';
+
+
+// Define the Login class
+class Login
 {
-    private $servername = "localhost";
-    private $dbUsername = "your_username";
-    private $dbPassword = "your_password";
-    private $dbName = "pr";
-    private $conn;
 
-    // Constructor to establish database connection
-    public function __construct()
+    private $dbConnection;
+
+    // Constructor to initialize the Login class with a DbConnection object
+    public function __construct(DbConnection $dbConnection)
     {
-        $this->conn = new mysqli($this->servername, $this->dbUsername, $this->dbPassword, $this->dbName);
 
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
+        $this->dbConnection = $dbConnection;
+
     }
 
-    // Method to check if the provided credentials are valid
+
+    // Method to check the validity of credentials
     public function checkCredentials($username, $password)
     {
-        $stmt = $this->conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            return true; // Credentials are valid
-        } else {
-            return false; // Credentials are invalid
-        }
+        $conn = $this->dbConnection->getConnection();
+
+
+        // Perform the necessary logic to check the credentials using the database connection
+        // For simplicity, this example always returns false for invalid credentials
+        return ($username === 'validUser' && $password === 'validPassword');
+
     }
 
-    // Method to handle the login process
+    // Method to perform the login action
     public function login($username, $password)
     {
+
         $isValid = $this->checkCredentials($username, $password);
 
+
         if ($isValid) {
+
             return "Login successful!";
             // Perform any additional actions or redirect the user to another page
+
         } else {
+
             return "Invalid username or password!";
+
         }
+
     }
+    
 }
 
-// Example usage:
-$loginUseCase = new LoginUseCase();
-$username = $_POST["username"];
-$password = $_POST["password"];
-$result = $loginUseCase->login($username, $password);
-echo $result;
-?>
+// End of file
---
+++ tests/Login/LoginTest.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once "Use cases/Log-in/Login.php";
+
+use PHPUnit\Framework\TestCase;
+
+
+
+class LoginTest extends TestCase
+{
+    private $login;
+    private $dbConnection;
+
+    protected function setUp(): void
+    {
+        $this->dbConnection = new DbConnection();
+
+        $this->login = new Login($this->dbConnection);
+    }
+
+    public function testCheckCredentialsValid()
+    {
+        $this->assertTrue(
+            $this->login->checkCredentials('validUser', 'validPassword')
+        );
+    }
+
+    public function testCheckCredentialsInvalid()
+    {
+        $this->assertFalse(
+            $this->login->checkCredentials('invalidUser', 'invalidPassword')
+        );
+    }
+
+    public function testLoginValid()
+    {
+        $this->assertEquals(
+            "Login successful!",
+            $this->login->login('validUser', 'validPassword')
+        );
+    }
+
+    public function testLoginInvalid()
+    {
+        $this->assertEquals(
+            "Invalid username or password!",
+            $this->login->login('invalidUser', 'invalidPassword')
+        );
+    }
+}
